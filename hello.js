2187ebf3e3d9ed6c3bcd946d51f47196708bcb78
tusharbisen <84771382+tusharbisen@users.noreply.github.com>
2024-02-24 09:32:41 -0800

    this is a first code

---
+++ hello.js
@@ -0,0 +1,1 @@
+console.log("Tejasṇ")
---
+++ test.js
@@ -0,0 +1,7 @@
+// console.log("Tushar");
+const aacoount = 1211
+console.log(aacoount);
+var name =  "tushar"
+let number = 1121
+console.log(number)
+console.log(name)
